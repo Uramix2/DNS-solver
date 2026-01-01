0c723eff0af7d57f6abde0a7ca8101f9962588f0
Uramix2 <axel.michellet@gmail.com>
2026-01-01 21:39:28 +0100

    rich display + parser

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
+import argparse
 import dns.resolver
 import sys
+
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
 
 liste = [
     "A",
@@ -12,17 +17,26 @@ liste = [
     "CAA",
     "PTR",
     "SRV",
-    "NAPTR",
-    "DNSKEY",
     "DS",
-    "RRSIG",
-    "NSEC",
+
+    
 ]
-def dns_type(domain, liste):
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("domain")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    return parser.parse_args()
+
+
+def dns_type(domain, liste,verbose=False):
     """
     Permet d'afficher les types de dns via brute force
     """
     resolver = dns.resolver.Resolver()
+    resolver.lifetime = 3.0 #  2 secondes max par req
     result = {}
     for e in liste:
         try:
@@ -31,14 +45,58 @@ def dns_type(domain, liste):
             if values:
                 result[e] = values
         except Exception as ex:
-            print(e, "error : -->", type(ex).__name__)
+            if verbose:
+                print(e, "error : -->", type(ex).__name__)
             continue
     return result
 
 
+def display_result(domain,results,console):
+    table = Table(
+        title=f"Résultat pour {domain}",
+        show_header=True,
+        header_style="bold white on blue",
+        border_style="bright_black",
+        expand=True)
+    
+    table.add_column("Type",style="cyan")
+    table.add_column("Valeurs",style="Green",overflow="fold")
 
-imput = sys.argv[1]
-print(dns_type(imput,liste))
+    for types,val in results.items():
+        table.add_row(types, "\n".join(val))
+        table.add_section()
+    
+
+    console.print(table)
+
+
+def main():
+    args = parse_args()
+    domain= args.domain
+    console = Console()
+
+    console.print(Panel(
+            f"[bold blue]Liste les types et les informations trouvés pour le domaine [underline]{domain}[/underline][/bold blue]",
+            title="DNS tools",
+            border_style="blue",
+            padding=(1, 2),
+        ))
+    result = dns_type(domain,liste,verbose=args.verbose)
+
+    if result:
+        display_result(domain,result,console)
+    
+    else:
+        console.print("[red]domaine invalide[/red]")
+        
+    
+        
+
+if __name__ == "__main__":
+    main()
+
+
+
 
 
 
